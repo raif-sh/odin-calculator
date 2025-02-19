3ea45ba9c21b1e43157a1c73914d95907f023fd9
Raif Shareef <raif.sh94@gmail.com>
2025-02-19 14:31:07 +0800

    operator function logic to check operation and runs correct function

--- app.js
+++ app.js
@@ -18,10 +18,18 @@ function divide(foo, bar){
 function operate(operator, num, ber){
     console.log("starting operation for " + operator)
     if(operator === "+"){
-        return add(num, ber)
+        return add(num, ber);
+    } else if (operator === "-") {
+        return sub(num, ber);
+    } else if (operator === "*") {
+        return multiply(num, ber);
+    } else if (operator === "/") {
+        return divide(num, ber);
+    } else {
+        return "invalid"
     }
 }
 
-let test = operate("+", 3, 4)
+let test = operate("/", 3, 4)
 
 console.log(test)
