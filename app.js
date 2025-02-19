a6906597620074f6c8c94c9080ceb25dc05062c4
Raif Shareef <raif.sh94@gmail.com>
2025-02-19 12:59:01 +0800

    adding basic functions and testing operator function

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert("connected")
+
+function add(foo, bar){
+    return foo + bar;
+}
+
+function sub(foo, bar){
+    return foo - bar;
+}
+
+function multiply(foo, bar){
+    return foo * bar;
+}
+
+function divide(foo, bar){
+    return foo / bar;
+}
+
+function operate(operator, num, ber){
+    console.log("starting operation for " + operator)
+    if(operator === "+"){
+        return add(num, ber)
+    }
+}
+
+let test = operate("+", 3, 4)
+
+console.log(test)
